5a8a0209909771d01878b337051ebda7a7827951
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-10-14 18:55:47 +0200

    Place tower on top of tiles

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,7 +63,8 @@ public class GameManager : MonoBehaviour
                             choosenTower = null;
                             return;
                         }
-                        Vector3 towerPos = new Vector3(hit.transform.position.x, hit.transform.position.y + 0.4f, hit.transform.position.z);
+                        Debug.Log(choosenTower.gameObject.transform.localScale.y);
+                        Vector3 towerPos = new Vector3(hit.transform.position.x, hit.transform.localScale.y / 2 + 0.1f, hit.transform.position.z);
                         Instantiate(choosenTower, towerPos, Quaternion.identity, towers.transform);
                         UpdateGold(-choosenTower.GetCost());
                     }
